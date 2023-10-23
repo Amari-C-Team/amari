157d697c729b70ff3f70363ef158d6a9b8fd63df
itsregalo <itsregalo047@gmail.com>
2023-10-23 14:03:08 +0300

    fixed loader issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+
+import './assets/css/animate.min.css'
+import './assets/css/bootstrap.min.css'
+import './assets/css/slicknav.min.css'
+import './assets/css/owl.carousel.css'
+import './assets/css/main.css'
+import './assets/css/responsive.css'
+
 import Home from './pages/Home'
 import ContactUs from './pages/ContactUs'
 import Projects from './pages/Projects'
